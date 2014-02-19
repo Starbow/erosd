990339765f956efdd2d5f7d26d66718fece7b9ed
Graylin Kim <graylin.kim@gmail.com>
2014-02-19 00:48:43 -0500

    Connect to a python daemon for replay parsing.
    Example using port 54321:
      * Add python.port = :54321 to eros.cfg
      * Run python sc2json.py 54321 to listen on port 54321.
      * Start erosd
    
    Tested in isolation but not with full running server process.

--- sc2json.py
+++ sc2json.py
@@ -2,22 +2,30 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals, division
 
+import SocketServer
 import sc2reader
 from sc2reader.factories.plugins.replay import toJSON
+sc2reader.register_plugin("Replay", toJSON(encoding='UTF-8', indent=None))
+
+
+class ReplayParser(SocketServer.StreamRequestHandler):
+    def handle(self):
+        path = self.rfile.readline().strip()
+        print("Parsing replay file: "+path)
+        json = sc2reader.load_replay(path, load_level=2)
+        self.wfile.write(json+"\n\n")
 
 
 def main():
     import argparse
-    parser = argparse.ArgumentParser(description="Prints replay data to a json string.")
-    parser.add_argument('--indent', '-i', type=int, default=None, help="The per-line indent to use when printing a human readable json string")
-    parser.add_argument('--encoding', '-e', type=str, default='UTF-8', help="The character encoding use..")
-    parser.add_argument('path', metavar='path', type=str, nargs=1, help="Path to the replay to serialize.")
+    parser = argparse.ArgumentParser(description="Listens on .")
+    parser.add_argument('PORT', metavar='port', type=int, nargs=1, help="The port to listen on.")
     args = parser.parse_args()
 
-    factory = sc2reader.factories.SC2Factory()
-    factory.register_plugin("Replay", toJSON(encoding=args.encoding, indent=args.indent))
-    replay_json = factory.load_replay(args.path[0])
-    print(replay_json)
+    HOST, PORT = 'localhost', args.PORT[0]
+    print("Listening on: {}:{}".format(HOST, PORT))
+    server = SocketServer.TCPServer((HOST, PORT), ReplayParser)
+    server.serve_forever()
 
 if __name__ == '__main__':
     main()
