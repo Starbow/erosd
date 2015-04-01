b7b707459964fd2471bcab2ac80437dadb9657cc
Sebastian SoaH <sikian@gmail.com>
2015-04-01 20:16:31 +0200

    Finish locale, guessing it's actually working

--- web/src/app/locale.js
+++ web/src/app/locale.js
@@ -3,19 +3,35 @@
 var module = angular.module('erosApp');
 
 module.config(['$translateProvider', function ($translateProvider) {
-
     $translateProvider.translations('en', starbow.locale["en"]);
 	$translateProvider.translations('kr', starbow.locale["kr"]);
 
     $translateProvider.preferredLanguage('en');
+    // console.log($translateProvider.preferredLanguage())
+    
+}]);
+
+module.controller('LocaleController', ['$scope', '$locale', '$translate', function($scope, $locale, $translate){
+	// alert($locale.id + ", "+$locale.id.split('-')[0])
+
+	var lang;
+	if($locale.id.split('-')[0] == 'kr'){
+		lang = 'kr'
+	}else{
+		lang = 'en'
+	}
+	$scope.setLanguage = lang;
+	$translate.use(lang);
 }]);
 
 module.directive('changeLanguage', function($translate){
 	return {
 		restrict: 'AC',
+		// controller: 'LocaleController',
 		link: function($scope, $elem, $attrs, $controller){
 			$elem.on('click', function(){
 				var lang = $attrs['lang']
+				$scope.setLanguage = lang
 				if(lang.length > 0){
 					$translate.use(lang);
 				}
